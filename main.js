29f4265ed941d5454f8a8904d897b3d742694a63
Daniel Drummond <dani.drummond.dev@gmail.com>
2025-11-14 12:12:59 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,105 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+const questionImg = document.getElementById('questionImg');
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+fetch('questions.json')
+  .then((res) => {
+    return res.json();
+  })
+  .then((loadedQuestions) => {
+    // console.log(loadedQuestions);
+    questions = loadedQuestions;
+
+    startGame();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+// Constants
+const CORRECT_BONUS = 1;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  // console.log(availableQuestions);
+  getNewQuestion();
+  game.classList.remove('hidden');
+  loader.classList.add('hidden');
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem('mostRecentScore', score);
+    // Go to the end game
+    return window.location.assign('/result');
+  }
+  questionCounter++;
+  progressText.innerText = `Pergunta ${questionCounter}/${MAX_QUESTIONS}`;
+  // Update Progress Bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  console.log(questionIndex);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+  //question.style.backgroundImage = currentQuestion.imgUrl;
+  //question.setAttribute("style", "background-image: url(" + currentQuestion.imgUrl + ")")
+  questionImg.setAttribute('src', currentQuestion.imgUrl);
+  console.log(currentQuestion.imgUrl);
+
+  choices.forEach((choice) => {
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion['choice' + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener('click', (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+    if (classToApply === 'correct') {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.classList.remove('button__secondary');
+    selectedChoice.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.classList.remove(classToApply);
+      selectedChoice.classList.add('button__secondary');
+      getNewQuestion();
+    }, 3000);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
